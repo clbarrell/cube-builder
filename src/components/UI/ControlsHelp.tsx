ec5ce7f6f3643c4f65dd6293d3050ec75b89e2fa
Chris <clbarrell@gmail.com>
2025-03-01 21:32:23 +1100

    update

--- src/components/UI/ControlsHelp.tsx
+++ src/components/UI/ControlsHelp.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ControlsHelp: React.FC = () => {
   return (
-    <div className="fixed top-0 left-0 p-4 text-white bg-black/80">
+    <div className="fixed top-0 left-0 p-4 text-sm select-none text-white bg-gray-900/80 rounded-br-lg">
       <p>WASD/Arrow Keys: Move</p>
       <p>Mouse: Look around</p>
       <p>Space: Move up</p>
--- src/components/environment/Floor.tsx
+++ src/components/environment/Floor.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 const Floor: React.FC = () => {
+  const floorSizeWidth = 50;
+  const floorSizeHeight = 50;
   return (
     <>
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
-        <planeGeometry args={[50, 50]} />
+        <planeGeometry args={[floorSizeWidth, floorSizeHeight]} />
         <meshStandardMaterial color="#555555" />
       </mesh>
       <gridHelper
-        args={[50, 50, "#888888", "#444444"]}
+        args={[floorSizeWidth, floorSizeHeight, "#888888", "#444444"]}
         position={[0, 0.01, 0]}
         rotation={[0, 0, 0]}
       />
