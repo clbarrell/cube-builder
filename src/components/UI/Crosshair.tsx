cb36fab583066021f8b366cc73c35bad5425586a
Chris <clbarrell@gmail.com>
2025-03-01 23:31:00 +1100

    fade cursor

--- src/components/UI/Crosshair.tsx
+++ src/components/UI/Crosshair.tsx
@@ -5,9 +5,9 @@ const Crosshair: React.FC = () => {
     <div className="fixed inset-0 flex items-center justify-center pointer-events-none">
       <div className="relative w-5 h-5">
         {/* Horizontal line */}
-        <div className="absolute top-1/2 left-0 w-full h-0.5 bg-white opacity-70 -translate-y-1/2"></div>
+        <div className="absolute top-1/2 left-0 w-full h-0.5 bg-white opacity-50 -translate-y-1/2"></div>
         {/* Vertical line */}
-        <div className="absolute top-0 left-1/2 h-full w-0.5 bg-white opacity-70 -translate-x-1/2"></div>
+        <div className="absolute top-0 left-1/2 h-full w-0.5 bg-white opacity-50 -translate-x-1/2"></div>
       </div>
     </div>
   );
