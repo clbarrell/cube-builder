c5126661b24cbd0d13499993595c1a44fffde52a
Chris <clbarrell@gmail.com>
2025-03-06 10:48:29 +1100

    faded text

--- src/components/UI/PositionBar.tsx
+++ src/components/UI/PositionBar.tsx
@@ -10,7 +10,7 @@ const PositionBar: React.FC = () => {
   }
 
   return (
-    <div className="absolute text-left top-0 left-1/2 -translate-x-1/2 bg-black/70 text-white px-4 py-1 rounded-b-md">
+    <div className="absolute text-left top-0 left-1/2 -translate-x-1/2 bg-black/75 text-white/80 px-4 py-1 rounded-b-md">
       <div className="text-xs font-medium font-mono flex items-center gap-2">
         <div>Your position:</div>
         <div>{player.position.x.toFixed(0)},</div>
